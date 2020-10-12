46922977db3c5cfddbb682724a3fe130bce6ffe3
Danhout <superdaniil111@mail.ru>
2020-10-12 17:41:18 +0300

    Add log4j-2 logging and change errors.

--- server/src/main/java/database/Configs.java
+++
@@ -1,8 +0,0 @@
-package database;
-
-public class Configs {
-    protected String dbHost = "localhost";//pg
-    protected String dbPort = "5432";
-    protected String dbName = "users";//studs
-    protected String connectionString = String.format("jdbc:postgresql://%s:%s/%s", dbHost, dbPort, dbName);
-}
