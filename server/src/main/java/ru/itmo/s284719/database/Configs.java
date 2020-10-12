8d2b3938f021144433ea72b6f6ff6e2498719371
Danhout <superdaniil111@mail.ru>
2020-10-12 21:20:45 +0300

    Add log4j-2 logging without JVM parameters and to JRE (but in JRE need to putting directory settings/log4j2.xml)

--- server/src/main/java/ru/itmo/s284719/database/Configs.java
+++ server/src/main/java/ru/itmo/s284719/database/Configs.java
@@ -1,8 +1,8 @@
 package ru.itmo.s284719.database;
 
 public class Configs {
-    protected String dbHost = "localhost";//pg
+    protected String dbHost = "pg";//pg localhost
     protected String dbPort = "5432";
-    protected String dbName = "users";//studs
+    protected String dbName = "studs";//studs users
     protected String connectionString = String.format("jdbc:postgresql://%s:%s/%s", dbHost, dbPort, dbName);
 }
